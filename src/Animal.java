d59f91329cb05ad46bf7c34d96b4b8e4bd31e716
szumek <szum97@gmail.com>
2021-02-28 13:42:34 +0100

    Zadanie2

---
+++ src/Animal.java
@@ -0,0 +1,11 @@
+public class Animal{
+    private String name;
+    private String species;
+    private int age;
+
+    public Animal(String name, String species, int age) {
+        this.name = name;
+        this.species = species;
+        this.age = age;
+    }
+}
---
+++ src/Phone.java
@@ -0,0 +1,9 @@
+public class Phone {
+    private String model;
+    private String company;
+
+    public Phone(String model, String company) {
+        this.model = model;
+        this.company = company;
+    }
+}
