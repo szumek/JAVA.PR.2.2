9abd79a04d1ac265ce611d67c8881dfd0ac542bb
szumek <szum97@gmail.com>
2021-02-28 13:30:43 +0100

    Zadanie2

---
+++ src/Human.java
@@ -0,0 +1,13 @@
+public class Human {
+    private String firstName;
+    private String lastName;
+    private String position;
+    private double salary;
+
+    public Human(String firstName, String lastName, String position, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.salary = salary;
+    }
+}
