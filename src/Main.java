f9a96c87df34432578b124a0fc350236866f3565
szumek <szum97@gmail.com>
2021-02-28 13:41:57 +0100

    Zadanie2

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+
+        Human human1 = new Human("Janusz", "Kowalski", "Tester", 12000);
+        Human human2 = new Human("Andrzej", "Kowalczyk", "Team Manager", 18000);
+
+
+        Phone phone1 = new Phone("Galaxy S III", "Samsung");
+
+        Animal animal1 = new Animal("Zefir", "Parrot", 5);
+        Animal animal2 = new Animal("Zeus", "Dog", 3);
+
+
+    }
+}
